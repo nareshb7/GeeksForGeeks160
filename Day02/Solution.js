14db4a5de09b397c9d7cef951ee2f7b8f506e5a4
nareshb7 <nareshbjava7@gmail.com>
2024-11-16 22:57:45 +0530

    Move All Zeroes to End

--- Day02/Solution.js
+++ Day02/Solution.js
@@ -1,27 +1,39 @@
 
 class Solution {
   pushZerosToEnd(arr) {
-    // to track the zeros
-    let left = -1;
-    for (let i = 0; i < arr.length; i++) {
-      
-      // cond1: curr val is 0 and left not found
-      if (arr[i] === 0 && left === -1) {
-        left = i;
-      } else if (left !== -1 && arr[i] !== 0) { 
-        // cond2: we are already found zero and curr val is not zero means need to swap
-        arr[left] = arr[i];
-        arr[i] = 0;
 
-        // update left pointer to next zero value position
-        while (left < i) {
-          if (arr[left] === 0) {
-            break;
-          }
-          left++;
-        }
+    // Instead of moving zeros we are swapping non-zero values
+    let left = 0; // Pointer for the position to place non-zero elements
+    for (let right = 0; right < arr.length; right++) {
+      if (arr[right] !== 0) {
+        // Swap non-zero element with the element at 'left'
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
       }
     }
+
+
+    // // to track the zeros
+    // let left = -1;
+    // for (let i = 0; i < arr.length; i++) {
+      
+    //   // cond1: curr val is 0 and left not found
+    //   if (arr[i] === 0 && left === -1) {
+    //     left = i;
+    //   } else if (left !== -1 && arr[i] !== 0) { 
+    //     // cond2: we are already found zero and curr val is not zero means need to swap
+    //     arr[left] = arr[i];
+    //     arr[i] = 0;
+
+    //     // update left pointer to next zero value position
+    //     while (left < i) {
+    //       if (arr[left] === 0) {
+    //         break;
+    //       }
+    //       left++;
+    //     }
+    //   }
+    // }
   }
 }
 
