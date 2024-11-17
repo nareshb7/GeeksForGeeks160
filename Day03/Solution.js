0d0c9a6acb31f4061eebb32a686b6d97c70b6903
nareshb7 <nareshbjava7@gmail.com>
2024-11-17 19:34:07 +0530

    approach updated

--- Day03/Solution.js
+++ Day03/Solution.js
@@ -1,14 +1,15 @@
+
 class Solution {
   // Function to reverse the array.
   reverseArray(arr) {
-      // your code here
-      let l =0; 
-      let r = arr.length -1;
-      while (l <r) {
-          [arr[l], arr[r]] = [arr[r], arr[l]]
-          l++;
-          r--;
-      }
+    // two pointer technique
+    let l = 0;
+    let r = arr.length - 1;
+    while (l < r) {
+      [arr[l], arr[r]] = [arr[r], arr[l]];
+      l++;
+      r--;
+    }
   }
 }
 
@@ -16,5 +17,5 @@ class Solution {
 
 // Input: arr = [1, 4, 3, 2, 6, 5]
 // Output: [5, 6, 2, 3, 4, 1]
-// Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, 
+// Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position,
 // the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.
