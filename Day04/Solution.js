b2da96ec28411fc896e4eb3144333673721ea9e1
nareshb7 <nareshbjava7@gmail.com>
2024-11-19 22:48:41 +0530

    Juggling Algo added for reverse array

--- Day04/Solution.js
+++ Day04/Solution.js
@@ -25,6 +25,40 @@ class Solution {
   }
 }
 
+// Juggling Algorithm;
+// class Solution {
+//   // Function to rotate an array by d elements in counter-clockwise direction.
+//   rotateArr(arr, d) {
+//       // code here
+//       const gcd =(a,b) => {
+//           while (b != 0) {
+//               let temp = b;
+//               b = a%b;
+//               a = temp;
+//           }
+//           return a
+//       }
+//       const n = arr.length;
+//       d = d%n;
+//       const cycles = gcd(n, d)
+      
+//       for (let i=0;  i<cycles; i++) {
+//           let currentEl = arr[i];
+//           let current = i;
+          
+//           while (true) {
+//               let next = (current +d) %n;
+//               if (next === i) {
+//                   break;
+//               }
+//               arr[current] = arr[next];
+//               current = next;
+//           }
+//           arr[current] = currentEl
+//       }
+//   }
+// }
+
 // Rotate Array
 
 // Given an unsorted array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer.
