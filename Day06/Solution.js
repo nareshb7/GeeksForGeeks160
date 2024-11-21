74c765d6193dbaf468dd7bb9cbaf6200bf9bc85b
nareshb7 <nareshbjava7@gmail.com>
2024-11-21 22:11:22 +0530

    Day 06 updated

--- Day06/Solution.js
+++ Day06/Solution.js
@@ -1,23 +1,20 @@
 class Solution {
   // Function to find the majority elements in the array
   findMajority(arr) {
-    // Your code goes here
-    let ele1 = 0;
-    let ele2 = 0;
-    let cnt1 = 0;
-    let cnt2 = 0;
+    let ele1 = 0, ele2 = 0;
+    let cnt1 = 0, cnt2 = 0;
     const n = arr.length;
     for (let i = 0; i < n; i++) {
-      const val = arr[i];
-      if (ele1 === val) {
+      const curr = arr[i];
+      if (ele1 === curr) {
         cnt1++;
-      } else if (ele2 === val) {
+      } else if (ele2 === curr) {
         cnt2++;
       } else if (cnt1 === 0) {
-        ele1 = val;
+        ele1 = curr;
         cnt1 = 1;
       } else if (cnt2 === 0) {
-        ele2 = val;
+        ele2 = curr;
         cnt2 = 1;
       } else {
         cnt1--;
@@ -25,15 +22,13 @@ class Solution {
       }
     }
 
-    cnt1 = 0;
-    cnt2 = 0;
-
+    cnt1 = 0, cnt2 = 0;
     for (const x of arr) {
       if (ele1 === x) cnt1++;
       if (ele2 === x) cnt2++;
     }
-    let res = [];
 
+    let res = [];
     if (cnt1 > n / 3) res.push(ele1);
     if (cnt2 > n / 3) res.push(ele2);
 
